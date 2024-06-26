7fa6aaef34decf92a7e2494ada50de674d775f53
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-06-26 10:10:38 +0200

    updated version and package init

---
+++ pyroll/equivalent-karman/__init__.py
@@ -0,0 +1,12 @@
+import importlib.util
+from . import roll_pass
+
+VERSION = "2.1.0"
+
+REPORT_INSTALLED = bool(importlib.util.find_spec("pyroll.report"))
+
+if REPORT_INSTALLED:
+    from . import report
+    import pyroll.report
+
+    pyroll.report.plugin_manager.register(report)
